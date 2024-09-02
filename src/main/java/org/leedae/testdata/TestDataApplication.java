c09370148b99c8eb64689cde789a5ff95ca8cbed
idaejin <ldj4241@naver.com>
2024-09-02 16:54:40 +0900

    #5 - 스프링 부트 프로젝트 초기화
    인텔리제이에서 Spring Initailizr를 통해 프로잭트를 초기화하고,
    당장 필요할 것으로 예상되는 디펜던시를 추가함.

---
+++ src/main/java/org/leedae/testdata/TestDataApplication.java
@@ -0,0 +1,13 @@
+package org.leedae.testdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestDataApplication.class, args);
+    }
+
+}
