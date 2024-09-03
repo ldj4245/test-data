2ac418fe51098aa56c394c121f475efc19285458
idaejin <ldj4241@naver.com>
2024-09-03 23:31:46 +0900

    #9 - 가짜 데이터 도메인 설계
    자동차 이름과 같은 고유 명사처럼
    가짜 데이터를 사전식으로 저장해서 관리해야 할 경우를
    도메인으로 설계함

---
+++ src/main/java/org/leedae/testdata/domain/MockData.java
@@ -0,0 +1,14 @@
+package org.leedae.testdata.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MockData {
+
+    private String mockDatType;
+    private String mockDataValue;
+}
