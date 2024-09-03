52e48d3255f58b2169df0d6b41ceb9a8d42261e7
idaejin <ldj4241@naver.com>
2024-09-03 23:48:32 +0900

    #9 - MockData: MockDataType에 최근 enum 변경을 반영
    직전 작업 #8 에서 `nockDataType` 필드는
    enum으로 관리하게 되었음.
    이 변경을 `MockData` 설계에 반영함.

--- src/main/java/org/leedae/testdata/domain/MockData.java
+++ src/main/java/org/leedae/testdata/domain/MockData.java
@@ -3,12 +3,13 @@ package org.leedae.testdata.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.leedae.testdata.domain.constant.MockDataType;
 
 @Getter
 @Setter
 @ToString
 public class MockData {
 
-    private String mockDatType;
+    private MockDataType mockDatType;
     private String mockDataValue;
 }
