a2fb843fbedd6d9ff7fc3eeb8c7e87ef786d221e
idaejin <ldj4241@naver.com>
2024-09-07 23:16:56 +0900

    #16 - JPA 비지니스 로직용 사용자 쿼리 메소드와 테스트 추가
    실제로 서비스에서 이용할 것으로 예상하는
    쿼리 3종을 추가하고 각각 정상 시나리오 테스트.
    
    * 특정 사용자가 가지고 있는 테이블 스키마 전체 목록 조회
    * 특정 사용자와 특정 이름의 테이블 스키마 1개 조회
    * 특정 사용자와 특정 이름의 테이블 스키마 삭제

--- src/main/java/org/leedae/testdata/repository/TableSchemaRepository.java
+++ src/main/java/org/leedae/testdata/repository/TableSchemaRepository.java
@@ -1,7 +1,16 @@
 package org.leedae.testdata.repository;
 
 import org.leedae.testdata.domain.TableSchema;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TableSchemaRepository extends JpaRepository<TableSchema, Long> {
+
+    Page<TableSchema> findByUserId(String userId, Pageable pageable);
+    Optional<TableSchema> findByUserIdAndSchemaName(String userId, String schemaName);
+    void deleteByUserIdAndSchemaName(String userId, String schemaName);
+
 }
