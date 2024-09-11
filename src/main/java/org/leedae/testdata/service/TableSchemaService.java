9837f416210a8dcabacc0a9f93fd0b4a74b73fc3
idaejin <ldj4241@naver.com>
2024-09-11 21:03:50 +0900

    #29 - 개인 스키마 목록 읽어들이는 서비스 로직 구현
    실제 사용할 서비스에 페이징 정보는 필요하지 않을 것 같아서
    `Pageable.unpaged()`로 페이징 정보를 생략한
    `List` 메소드로 기능을 내보냈다.

---
+++ src/main/java/org/leedae/testdata/service/TableSchemaService.java
@@ -0,0 +1,33 @@
+package org.leedae.testdata.service;
+
+import lombok.RequiredArgsConstructor;
+import org.leedae.testdata.dto.TableSchemaDto;
+import org.leedae.testdata.repository.TableSchemaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class TableSchemaService {
+
+    private final TableSchemaRepository tableSchemaRepository;
+
+
+    @Transactional(readOnly = true)
+    public List<TableSchemaDto> loadMySchemas(String userId) {
+        return loadMySchemas(userId, Pageable.unpaged()).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<TableSchemaDto> loadMySchemas(String userId, Pageable pageable) {
+        return tableSchemaRepository.findByUserId(userId, pageable)
+                .map(TableSchemaDto::fromEntity);
+    }
+
+
+}
