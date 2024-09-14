5373e9ed8c5b729cab5abd0cc84391d185f4cc21
idaejin <ldj4241@naver.com>
2024-09-14 18:39:58 +0900

    #33 - 파일 출력기 인터페이스와 구현체 작성
    파일 출력기가 제공해야 할 기능을 인터페이스로 설계하고
    각`.csv`, `.tsv` 포맷에 대응할 수 있는
    파일 출력기를 구현.
    이 둘은 로직에 공통점이 많기 때문에
    추상 클래스로 공통 로직을 뽑아내어 확장하는 방식을 선택.

---
+++ src/main/java/org/leedae/testdata/service/exporter/DelimiterBasedFileExporter.java
@@ -0,0 +1,51 @@
+package org.leedae.testdata.service.exporter;
+
+import org.leedae.testdata.domain.constant.ExportFileType;
+import org.leedae.testdata.dto.SchemaFieldDto;
+import org.leedae.testdata.dto.TableSchemaDto;
+import org.springframework.stereotype.Component;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+public abstract class DelimiterBasedFileExporter implements MockDataFileExporter{
+
+
+    /**
+     * 파일 열 구분자로 사용할 문자열을 반환한다.
+     *
+     * @return 파일 열 구분자
+     */
+    public abstract String getDelimiter();
+
+    @Override
+    public String export(TableSchemaDto dto, Integer rowCount) {
+        StringBuilder sb = new StringBuilder();
+
+        // 헤더 만들기
+        sb.append(dto.schemaFields().stream()
+                        .sorted(Comparator.comparing(SchemaFieldDto::fieldOrder))
+                .map(SchemaFieldDto::fieldName)
+                .collect(Collectors.joining(getDelimiter()))
+        );
+
+        sb.append("\n");
+
+        //데이터 부분
+        IntStream.range(0,rowCount).forEach(i -> {
+            sb.append(dto.schemaFields().stream()
+                    .sorted(Comparator.comparing(SchemaFieldDto::fieldOrder))
+                    .map(field -> "가짜-데이터") //TODO: 구현할 것
+                    .map(v -> v == null ? "" : v)
+                    .collect(Collectors.joining(getDelimiter()))
+
+            );
+            sb.append("\n");
+        });
+
+        return sb.toString();
+
+    }
+}
---
+++ src/test/java/org/leedae/testdata/service/exporter/TSVFileExporterTest.java
@@ -0,0 +1,48 @@
+package org.leedae.testdata.service.exporter;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.leedae.testdata.domain.constant.ExportFileType;
+import org.leedae.testdata.domain.constant.MockDataType;
+import org.leedae.testdata.dto.SchemaFieldDto;
+import org.leedae.testdata.dto.TableSchemaDto;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("[Logic] TSV 파일 출력기 테스트")
+class TSVFileExporterTest {
+
+    private TSVFileExporter sut = new TSVFileExporter();
+
+    @DisplayName("테이블 스키마 정보와 행 수가 주어지면, TSV 형식의 문자열을 생성한다.")
+    @Test
+    void givenSchemaAndRowCount_whenExporting_thenReturnsTSVFormattedString(){
+        // Given
+         ExportFileType exportFileType = ExportFileType.TSV;
+         TableSchemaDto dto = TableSchemaDto.of(
+                 "test_schema",
+                 "uno",
+                 null,
+                 Set.of(
+                         SchemaFieldDto.of("id", MockDataType.ROW_NUMBER,1,0,null,null),
+                         SchemaFieldDto.of("name",MockDataType.NAME,2,0,null,null),
+                         SchemaFieldDto.of("created_at",MockDataType.DATETIME,5,0,null,null),
+                         SchemaFieldDto.of("age",MockDataType.NUMBER,3,0,null,null),
+                         SchemaFieldDto.of("car", MockDataType.CAR,4,0,null,null)
+                 )
+
+         );
+         int rowCount = 10;
+
+        // When
+        String result = sut.export(dto,rowCount);
+
+
+         // Then
+         assertThat(result).startsWith("id\tname\tage\tcar\tcreated_at");
+    }
+
+}
