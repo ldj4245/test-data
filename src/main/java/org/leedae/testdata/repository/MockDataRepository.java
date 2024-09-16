52bacf760bcef2b037387d58ef07565d94d4166d
idaejin <ldj4241@naver.com>
2024-09-16 19:37:13 +0900

    #34 - 잦은 select 쿼리 발생이 우려되는 곳에 캐시 적용
    부트에서 제공하는 Spring Cache Abstraction을 이용해
    간단하게 애노테이션 방식으로 캐시를 적용함.
    그 결과 50개의 조회 쿼리가 1개로 줄어든 것을 관찰 가능.

--- src/main/java/org/leedae/testdata/repository/MockDataRepository.java
+++ src/main/java/org/leedae/testdata/repository/MockDataRepository.java
@@ -2,11 +2,13 @@ package org.leedae.testdata.repository;
 
 import org.leedae.testdata.domain.MockData;
 import org.leedae.testdata.domain.constant.MockDataType;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface MockDataRepository extends JpaRepository<MockData,Long> {
+    @Cacheable("mockData")
     List<MockData> findByMockDataType(MockDataType mockDataType);
 
 }
